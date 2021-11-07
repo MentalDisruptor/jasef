e422c65c3d61f5c972e9f555125d9e2f48ee794d
Kai Stempfle <kai.stempfle@gmail.com>
2021-11-07 20:06:39 +0100

    fix: determine base path correctly

--- lib/src/main/java/org/jasef/framework/base/BasePath.java
+++ lib/src/main/java/org/jasef/framework/base/BasePath.java
@@ -1,11 +1,52 @@
 package org.jasef.framework.base;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.nio.file.Path;
+
+@Slf4j
 public class BasePath {
 
+    private String getPathToClass() {
+        return new File(
+                BasePath.class
+                        .getProtectionDomain()
+                        .getCodeSource()
+                        .getLocation()
+                        .getPath())
+                .getName();
+    }
+
+    private boolean isRunningFromJar() {
+        return getPathToClass().contains(".jar");
+    }
+
+    private Path getCurrentProjectDirectory() {
+        return new File(".").getAbsoluteFile().toPath().getParent();
+    }
+
+    private Path getCurrentJarDirectory() {
+        return new File(
+                BasePath.class
+                        .getProtectionDomain()
+                        .getCodeSource()
+                        .getLocation()
+                        .getPath()
+        ).toPath();
+    }
+
+    /**
+     * Gets the project base path
+     * @return
+     */
     public Path getBasePath() {
-        return Paths.get(System.getProperty("user.dir"));
+        if (isRunningFromJar()) {
+            log.debug("Running from JAR.");
+            return getCurrentJarDirectory();
+        } else {
+            log.debug("Not running from JAR.");
+            return getCurrentProjectDirectory();
+        }
     }
 }
--- lib/src/test/java/org/jasef/framework/base/BasePathTest.java
+++ lib/src/test/java/org/jasef/framework/base/BasePathTest.java
@@ -1,23 +1,25 @@
 package org.jasef.framework.base;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Paths;
 
+@Slf4j
 class BasePathTest implements WithAssertions {
 
     @Test
     void getBasePathTest() {
         BasePath basePath = new BasePath();
         assertThat(basePath.getBasePath())
+                .withFailMessage("Base path does not exist")
+                .exists();
+        assertThat(basePath.getBasePath())
                 .withFailMessage("Base path is not a directory")
                 .isDirectory();
         assertThat(basePath.getBasePath())
                 .withFailMessage("Base path is not readable")
                 .isReadable();
-        assertThat(basePath.getBasePath())
-                .withFailMessage("Base path does not have expected value")
-                .isEqualTo(Paths.get(System.getProperty("user.dir")));
     }
 }
